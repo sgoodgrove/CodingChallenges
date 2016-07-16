ec37b08323f3574fc1dc073301e747db3c2caa29
Steven Goodgrove <steven@goodgrove.co.uk>
2016-07-16 17:57:29 +0100

    Added challenge for parsing Roman numerals to integers

---
+++ RomanNumerals/RomanNumerals/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class Solution
+{
+	public int ParseRomanNumerals(string input)
+	{
+		var numeralValues = new Dictionary<char, int>
+		{
+			{ 'M', 1000 },
+			{ 'D', 500 },
+			{ 'C', 100 },
+			{ 'L', 50 },
+			{ 'X', 10 },
+			{ 'V', 5},
+			{ 'I', 1 }
+		};
+
+		Func<char, int> numVal = (n) => numeralValues [n];
+		var pairValues = input.Zip (input.Skip (1), (a,b) => new Tuple<int,int>(numVal(a), numVal(b)));
+		var values =
+			pairValues
+				.Select (p => p.Item1 < p.Item2 ? p.Item1 * -1 : p.Item1)
+				.Concat (new int[] {numVal(input[input.Length - 1])});
+
+		return values.Sum ();
+
+	}
+}
---
+++ RomanNumerals/RomanNumerals/Tests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+[TestFixture]
+public class SolutionTests
+{
+	[Test]
+	public void Test1()
+	{
+		var solution = new Solution ();
+
+		Assert.That (solution.ParseRomanNumerals ("I"), Is.EqualTo (1));
+	}
+
+	[Test]
+	public void Test1961()
+	{
+		var solution = new Solution();
+
+		Assert.That(solution.ParseRomanNumerals("MCMLXI"), Is.EqualTo(1961));
+	}
+
+	[Test]
+	public void Test1999()
+	{
+		var solution = new Solution();
+
+		Assert.That(solution.ParseRomanNumerals("MCMXCIX"), Is.EqualTo(1999));
+	}
+
+	[Test]
+	public void Test2016()
+	{
+		var solution = new Solution();
+
+		Assert.That(solution.ParseRomanNumerals("MMXVI"), Is.EqualTo(2016));
+	}
+}
